7e449b7dcb34ff3d07da60c61193ba28b67f0a03
Sidney Congard <sidney.congard@gmail.com>
2017-11-23 13:37:25 -0500

    added enum class flag operators

---
+++ include/flag_enums.hpp
@@ -0,0 +1,95 @@
+
+#pragma once
+
+#include <type_traits>
+#include <cstdint>
+
+
+namespace flag_operators {
+
+    namespace detail {
+
+        namespace impl {
+
+            template<class Enum, class Expr = void>
+            struct integer_t;
+
+            template<class Enum>
+            struct integer_t<Enum, std::enable_if_t<
+                    sizeof(Enum) == 1
+            >> {
+                using type = uint8_t;
+            };
+
+            template<class Enum>
+            struct integer_t<Enum, std::enable_if_t<
+                    sizeof(Enum) == 2
+            >> {
+                using type = uint16_t;
+            };
+
+            template<class Enum>
+            struct integer_t<Enum, std::enable_if_t<
+                    sizeof(Enum) == 3 || sizeof(Enum) == 4
+            >> {
+                using type = uint32_t;
+            };
+
+            template<class Enum>
+            struct integer_t<Enum, std::enable_if_t<
+                    sizeof(Enum) >= 5 && sizeof(Enum) <= 8
+            >> {
+                using type = uint64_t;
+            };
+
+        }
+
+        template<class Enum>
+        using integer_t = typename impl::integer_t<Enum>::type;
+
+        template<class Enum>
+        auto int_of(Enum e) {
+            return static_cast<detail::integer_t<Enum>>(e);
+        }
+        template<class Enum>
+        auto& int_ref_of(Enum e) {
+            return static_cast<detail::integer_t<Enum>&>(e);
+        }
+    }
+
+    template<class Enum>
+    Enum operator|(Enum lhs, Enum rhs) {
+        return static_cast<Enum>(detail::int_of(lhs) | detail::int_of(rhs));
+    }
+
+    template<class Enum>
+    Enum operator&(Enum lhs, Enum rhs) {
+        return static_cast<Enum>(detail::int_of(lhs) & detail::int_of(rhs));
+    }
+
+    template<class Enum>
+    Enum operator^(Enum lhs, Enum rhs) {
+        return static_cast<Enum>(detail::int_of(lhs) ^ detail::int_of(rhs));
+    }
+
+    template<class Enum>
+    Enum operator~(Enum e) {
+        return static_cast<Enum>(~detail::int_of(e));
+    }
+
+    template<class Enum>
+    Enum& operator|=(Enum& lhs, Enum rhs) {
+        return static_cast<Enum&>(detail::int_ref_of(lhs) |= detail::int_of(rhs));
+    }
+
+    template<class Enum>
+    Enum& operator&=(Enum& lhs, Enum rhs) {
+        return static_cast<Enum&>(detail::int_ref_of(lhs) &= detail::int_of(rhs));
+    }
+
+    template<class Enum>
+    Enum& operator^=(Enum& lhs, Enum rhs) {
+        return static_cast<Enum&>(detail::int_ref_of(lhs) ^= detail::int_of(rhs));
+    }
+
+}
---
+++ tests/tests_flag_enums.cpp
@@ -0,0 +1,25 @@
+
+#include "catch.hpp"
+
+#include <flag_enums.hpp>
+
+
+namespace {
+    enum class MyFlags : uint64_t {
+        None =  0ull,
+        All =   static_cast<uint64_t>(~0ull),
+        One =   1ull << 0,
+        Two =   1ull << 1,
+        Three = 1ull << 2,
+        Four =  1ull << 3,
+    };
+}
+
+TEST_CASE("flag_enums basics", "[flag_enums]") {
+    using namespace flag_operators;
+
+    auto flags = MyFlags::One | MyFlags::Four;
+
+    auto val = (uint64_t) (MyFlags::One) + (uint64_t) (MyFlags::Four);
+    REQUIRE(flags == (MyFlags) val);
+}
