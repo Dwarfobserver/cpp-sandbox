bf4a9aaee91288b4e2c2fc6903bb3ae4144a18cf
Sidney Congard <sidney.congard@gmail.com>
2018-01-21 15:26:49 -0500

    added move op count in movable_function tests

--- tests/tests_movable_function.cpp
+++ tests/tests_movable_function.cpp
@@ -13,23 +13,44 @@ TEST_CASE("movable_function type erasure", "[movable_function]") {
     inc_f(val);
     REQUIRE(val == 3);
 
-    std::string name{"yolandis"};
-    sc::movable_function<std::string()> make_name_f([name] {
-        return name;
+    auto pName = std::make_unique<std::string>("yolandis");
+    auto name = *pName;
+    sc::movable_function<std::string()> make_name_f(
+            [pName = std::move(pName)] {
+        return *pName;
     });
     REQUIRE(make_name_f() == name);
 }
 
-TEST_CASE("movable_function move operations", "[movable_function]") {
-    auto pVal = std::make_unique<int>(42);
-    sc::movable_function<int()> gen_int_f([pVal = std::move(pVal)] {
-        return (*pVal)++;
-    });
-    REQUIRE(gen_int_f.is_valid());
+namespace {
+    struct Dummy {
+        int moveCount;
+        int operator()() {
+            return moveCount;
+        }
 
-    sc::movable_function<int()> f_copy;
-    sc::swap(gen_int_f, f_copy);
-    REQUIRE(!gen_int_f);
-    REQUIRE(f_copy() == 42);
-    REQUIRE(f_copy() == 43);
+        Dummy() : moveCount(0) {}
+
+        Dummy(Dummy const&) = delete;
+        Dummy(Dummy&& d) noexcept :
+                moveCount(d.moveCount + 1) {
+            d.moveCount = 0;
+        }
+
+        Dummy& operator=(Dummy const&) = delete;
+        Dummy& operator=(Dummy&& d) noexcept {
+            moveCount = d.moveCount + 1;
+            d.moveCount = 0;
+        }
+    };
+}
+
+TEST_CASE("movable_function move operations", "[movable_function]") {
+    sc::movable_function<int()> wrapper;
+    wrapper = {Dummy{}};
+
+    sc::movable_function<int()> wrapper2;
+    wrapper2 = std::move(wrapper);
+
+    REQUIRE(wrapper2() == 1);
 }
